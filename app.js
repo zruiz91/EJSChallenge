dc9ab0860cd6b7f601ec887f1a621ab84b937a18
Zach Ruiz <powersbooth10@gmail.com>
2023-05-21 23:02:58 -0500

    finished ejs project.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,7 +17,6 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-
 
 
 //get request for home page
@@ -41,6 +41,32 @@ app.get('/contact', (req, res) => {
 app.get('/compose', (req, res) => {
   res.render('compose',)
 })
+
+app.get('/posts/:postName', (req, res) => {
+  // for (let i = 0; i < posts.length; i++) {
+  //   if (req.params.postName === posts[i].title) {
+  //     console.log("match found");
+  //   } else (
+  //     console.log("match not found")
+  //   )
+  // }
+
+  const requestedTitle = _.lowerCase(req.params.postName);
+
+  posts.forEach(post => {
+    const storedTitle =  _.lowerCase(post.title);
+
+
+
+    if (requestedTitle === storedTitle) {
+          res.render("post", {
+            title: post.title,
+            content: post.content,
+          });
+        }
+  })
+})
+
 
 app.post('/compose', (req, res) => {
 
